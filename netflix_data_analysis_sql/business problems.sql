75abd175b78bf85c4efcee8eb88cfd82e7ef0f6b
Glenda Tay <153255427+glenqa@users.noreply.github.com>
2025-02-13 23:38:02 +0800

    Create business problems.sql

---
+++ netflix_data_analysis_sql/business problems.sql
@@ -0,0 +1,31 @@
+Q1:Determining the number of Movies vs TV Shows
+
+Q2: Determining the most common rating for movies and TV Shows
+
+Q3: List all movies released in a specific year (eg 2020)
+
+Q4: Finding the top 5 countries with the most content on Netflix
+
+Q5: Identify the top 5 longest movie
+
+Q6: Find content added in the last 5 years
+
+Q7: Find all the movies/TV shows by director 'Rajiv Chilaka'!
+
+Q8: List all TV shows with more than 5 seasons
+
+Q9: Count the number of content items in each genre
+
+Q10: Find the total amount of content released in India for each year
+  
+Q11: List all movies that are documentaries
+
+Q12: Find all content without a director
+
+Q13: Find how many movies actor 'Salman Khan' appeared in last 10 years!
+
+Q14: Find the top 10 actors who have appeared in the highest number of movies produced in India.
+
+Q15: Categorize the content based on the presence of the keywords 'kill' and 'violence' in 
+the description field. Label content containing these keywords as 'Bad' and all other content as 'Good'.
+Count how many items fall into each category.
